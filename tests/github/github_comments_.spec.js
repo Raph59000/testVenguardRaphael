d1fd3f997660d19bb57198ed5f56df68373512db
inqom-visma-qa <noreply@github.com>
2026-03-17 14:33:44 +0100

    Add named empty test shells

--- tests/github/github_comments_.spec.js
+++ tests/github/github_comments_.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('../../fixtures/fixtures.js')
 
-test('after creating an issue via API and adding a comment via UI, the comment should be asserted via API', async ({ request, page, ids }) => {
-
+test('after creating an issue via API and commenting via UI, the comment should be asserted via API', async ({ request, page, ids }) => {
 })
--- tests/github/github_issues_.spec.js
+++ tests/github/github_issues_.spec.js
@@ -1,9 +1,7 @@
 const { test, expect } = require('../../fixtures/fixtures.js')
 
 test('after creating an issue via API, it should be visible in the UI', async ({ request, page, ids }) => {
-
 })
 
 test('after editing and closing an issue via UI, the persisted state should be asserted via API', async ({ request, page, ids }) => {
-
 })
